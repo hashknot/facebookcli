f2d856d2c16bfbe01e9f09c20a70e24ad39157ad
Jitesh <msg.jitesh@gmail.com>
2012-11-27 00:44:22 +0530

    Added Auth Code
    The code was written to get access_token directly without using CODE param.
    Since its not working, would have to do it using the CODE param.

--- facebook.js
+++ facebook.js
@@ -1,6 +1,7 @@
 
 var https = require('https');
 var fs = require('fs');
+var http = require('http');
 
 function Facebook(accessToken){
   this.accessToken = accessToken;
@@ -8,7 +9,8 @@ function Facebook(accessToken){
 	this.appId = manifest.appId;
   this.appSecret = manifest.appSecret;
   this.permissions = manifest.permissions;
-	this.appUrl = 'http://localhost:16792/';
+	this.appPort = manifest.appServerPort;
+	this.appUrl = 'http://localhost:'+ this.appPort + '/';
 }
 
 Facebook.prototype.query = function(query,callback){
@@ -20,7 +22,7 @@ Facebook.prototype.query = function(query,callback){
   connectOptions.path += encodeURIComponent(query) + '&' + this.accessToken;
 	var request = https.request(connectOptions, function(response){
 		if(response.statusCode == 400){
-			console.log("\n\nOops! Something went wrong\n\nHere's what I know :" + 
+			console.log("\n\nOops! Something went wrong\n\nHere's what I know :" +
 			"\n\nResponse HTTP Code : 400");
 			console.log(response.headers);
 		}
@@ -29,7 +31,7 @@ Facebook.prototype.query = function(query,callback){
 			response.on('data', function(d){ data += d;});
 			response.on('end',function(){ callback(data); });
 			response.on('close',function(){
-				 console.log("\n\nOops! Something went wrong\n");
+			  console.log("\n\nOops! Something went wrong\n");
 			});
 		}
 	});
@@ -37,24 +39,36 @@ Facebook.prototype.query = function(query,callback){
 };
 
 Facebook.prototype.getAuthUrl = function(){
-	var dialogUrl = 'https://www.facebook.com/dialog/oauth' + 
-	'?client_id=' + this.appId + 
-	'&redirect_uri=' + encodeURIComponent(this.appUrl) + 
-	'&scope=' + encodeURIComponent(this.permissions);
+	var dialogUrl = 'https://www.facebook.com/dialog/oauth' +
+	'?client_id=' + this.appId +
+	'&redirect_uri=' + encodeURIComponent(this.appUrl) +
+	'&scope=' + encodeURIComponent(this.permissions) +
+	'&response_type=token';
 	return dialogUrl;
 };
 
 Facebook.prototype.authorize = function(callback){
-	console.log('To authorize, visit : ' + this.getAuthUrl());
-	this.authResponseWait(this.acquireAccessToken);
+	authCompleteCallback = callback;
+	console.log("To authorize, visit : \n\n\t" + this.getAuthUrl());
+	this.createAppServer(this.acquireAccessToken);
 };
 
-Facebook.prototype.authResponseWait = function(callback){
-
+Facebook.prototype.createAppServer = function(responseCallback){
+	console.log("\n\nStarting the App Server on localhost:" + this.appPort);
+	server=http.createServer(responseCallback).listen(this.appPort,'127.0.0.1');
 };
 
-Facebook.prototype.acquireAccessToken = function(code,callback){
-
+Facebook.prototype.acquireAccessToken = function(req,res){
+	console.log("Request : " + req.url);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	if(req.url.indexOf('access_token=') == -1 ){
+		res.end('App is waiting for a valid access_token');
+		return;
+	}
+	this.accessToken = req.url.match(/[^#.]*$/);
+	res.end('Got an Accesstoken. Shutting down the server now');
+	server.close();
+	authCompleteCallback(this.accessToken);
 };
 
 module.exports = Facebook;
