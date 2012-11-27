f1d0154cbfb1c02d0a0fe26447e6271e85ec0a09
Jitesh <msg.jitesh@gmail.com>
2012-11-27 11:58:09 +0530

    Access_token authentication successful.
    Can successfully authorize the user and get the access_token.
    TODO:
    1. Save the access_token.
    2. Encrypt the access_token.

--- facebook.js
+++ facebook.js
@@ -42,8 +42,7 @@ Facebook.prototype.getAuthUrl = function(){
 	var dialogUrl = 'https://www.facebook.com/dialog/oauth' +
 	'?client_id=' + this.appId +
 	'&redirect_uri=' + encodeURIComponent(this.appUrl) +
-	'&scope=' + encodeURIComponent(this.permissions) +
-	'&response_type=token';
+	'&scope=' + encodeURIComponent(this.permissions);
 	return dialogUrl;
 };
 
@@ -55,20 +54,53 @@ Facebook.prototype.authorize = function(callback){
 
 Facebook.prototype.createAppServer = function(responseCallback){
 	console.log("\n\nStarting the App Server on localhost:" + this.appPort);
+	that=this;
 	server=http.createServer(responseCallback).listen(this.appPort,'127.0.0.1');
 };
 
 Facebook.prototype.acquireAccessToken = function(req,res){
 	console.log("Request : " + req.url);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	if(req.url.indexOf('access_token=') == -1 ){
+	if(req.url.indexOf('code=') == -1 ){
 		res.end('App is waiting for a valid access_token');
 		return;
 	}
-	this.accessToken = req.url.match(/[^#.]*$/);
-	res.end('Got an Accesstoken. Shutting down the server now');
+	res.end('Access_token acquired. You can close this tab/window');
+	req.connection.destroy();
+	var code = req.url.match(/[^=.]*$/)[0];
 	server.close();
-	authCompleteCallback(this.accessToken);
+
+	/*
+	* Got the Code. Now exchange it for access_token
+	*/
+	var connectOpts = { host : 'graph.facebook.com' ,
+	port : 443,
+	method : 'GET'
+	};
+	connectOpts.path = '/oauth/access_token' +
+	'?client_id=' + that.appId +
+	'&redirect_uri=' + encodeURIComponent(that.appUrl) +
+	'&client_secret=' + encodeURIComponent(that.appSecret) +
+	'&code=' + code;
+
+	var request = https.request(connectOpts, function(response){
+		if(response.statusCode == 400){
+			console.log("\n\nOops! Something went wrong\n\nHere's what I know :" +
+			"\n\nResponse HTTP Code : 400");
+			console.log(response.headers);
+		}
+		else{
+			var data='';
+			response.on('data', function(d){ data += d;});
+			response.on('end',function(){
+				authCompleteCallback(data);
+			});
+			response.on('close',function(){
+				console.log("\n\nOops! Something went wrong\n");
+			});
+		}
+	});
+	request.end();
 };
 
 module.exports = Facebook;
